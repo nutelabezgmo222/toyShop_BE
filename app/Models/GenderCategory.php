a5f89f539aeba53bcad63f404daeda4d5509d315
Maksym Solodovnykov <solodovnikov.maksim123@gmail.com>
2022-07-12 22:42:00 +0300

    TSA-57: Updated genders api to handle subCategory

--- app/Models/GenderCategory.php
+++ app/Models/GenderCategory.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Toy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class GenderCategory extends Model
 {
     use HasFactory;
+
+    public function toys() {
+        return $this->hasMany(Toy::class, 'GenderCategory_id');
+    }
 }
